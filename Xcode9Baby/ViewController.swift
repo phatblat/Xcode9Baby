856a17ba0628de27b4dab83a7d9ab2348b8217ef
Ben Chatelain <ben@octop.ad>
2017-06-06 09:29:30 -0600

    Add project and workspace

---
+++ Xcode9Baby/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Xcode9Baby
+//
+//  Created by Ben Chatelain on 6/5/17.
+//  Copyright © 2017 Log Dash G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        _ = ""
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
